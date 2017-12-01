607ac147b14ebb41a763d04f7513988922ba98b6
luckyling <1978467950@qq.com>
2017-12-01 20:53:08 +0800

    first

--- webpack/dist/index.js
+++ webpack/dist/index.js
@@ -2,4 +2,4 @@
  * Created by asus1 on 2017/12/1.
  */
 var oDiv=document.getElementById("div");
-oDiv.innerHTMl="hello world!!";
+oDiv.innerHTML="hello world1233!!";
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,6 +2,7 @@
  * Created by asus1 on 2017/12/1.
  */
 const path =require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports={
     entry:{
         entry:"./dist/index.js"
@@ -9,5 +10,20 @@ module.exports={
     output:{
         path:path.resolve(__dirname,'src'),
         filename:'[entry].js'
-    }
-}
+    },
+    devServer:{
+        contentBase:path.resolve(__dirname,'src'),
+        host:'127.0.0.1',
+        compress:true,
+        port:8081
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            minify: {
+                removeAttributeQuotes: true
+            },
+            hash: true,
+            template: './dist/index.html'
+        })
+    ]
+};
