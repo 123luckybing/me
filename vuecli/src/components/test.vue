72ca87778a4d64bed9d7c235505cef2a4add6a4f
luckyling <1978467950@qq.com>
2017-12-06 18:28:19 +0800

    first

---
+++ vuecli/src/components/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+<script>
+export default{
+  data(){
+    return{
+      msg:"测试组件"
+    }
+  }
+}
+</script>
+<style scoped>
+  h1{
+    background:red;
+  }
+</style>
