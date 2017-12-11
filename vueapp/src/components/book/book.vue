0c5a6b0dc68a9dfb74670b6ba9455de13fbd0f0c
luckyling <1978467950@qq.com>
2017-12-11 21:01:34 +0800

    first

--- vueapp/src/components/book/book.vue
+++ vueapp/src/components/book/book.vue
@@ -3,7 +3,13 @@
       <CommonHeader bg='rgb(33, 150, 243)'>
           <span slot='title'>Book</span>
       </CommonHeader>
+      <swipe class="my-swipe">
+        <swipe-item class="slide1">
 
+        </swipe-item>
+        <swipe-item class="slide2"></swipe-item>
+        <swipe-item class="slide3"></swipe-item>
+      </swipe>
       <CommonFooter bgc='rgb(33, 150, 243)'>
       </CommonFooter>
     </div>
@@ -11,15 +17,41 @@
 <script>
   import CommonHeader from '../common/header'
   import CommonFooter from '../common/footer'
+  require('vue-swipe/dist/vue-swipe.css');
+  import { Swipe, SwipeItem } from 'vue-swipe';
 export default{
     components:{
       CommonFooter,
-      CommonHeader
+      CommonHeader,
+      'swipe': Swipe,
+      'swipe-item': SwipeItem
     }
 }
 
 </script>
 
 <style scoped>
+.my-swipe {
+  height: 200px;
+  color: #fff;
+  font-size: 30px;
+  text-align: center;
+  margin:1rem 0;
 
+}
+
+.slide1 {
+  background-image: url('https://img3.doubanio.com/lpic/s24468373.jpg');
+  color: #fff;
+}
+
+.slide2 {
+  background-image: url('https://img3.doubanio.com/lpic/s6989253.jpg');
+  color: #000;
+}
+
+.slide3 {
+  background-image: url('https://img3.doubanio.com/lpic/s27102925.jpg');
+  color: #fff;
+}
 </style>
