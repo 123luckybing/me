f08d7c3e16df9466b5ad67d7a1e0f0901a21951f
luckyling <1978467950@qq.com>
2018-03-11 15:09:41 +0800

    person-website 2.0

--- 个人网站/index.js
+++ 个人网站/index.js
@@ -1,65 +1,13 @@
-(function(){
-    var oNav=document.getElementById("nav");
-    window.onscroll=function(){
-        var t=document.documentElement.scrollTop||document.body.scrollTop;
-        if(t>50){
-            oNav.style.position="fixed";
-            oNav.style.top="0";
-            oNav.style.opacity="0.8";
-        }else{
-            oNav.style.position="static";
-            oNav.style.opacity="1";
-        }
-    };
-    var oFixed=document.getElementById("fixed");
-    oFixed.onclick=function() {
-        var timer = setInterval(function () {
-            var iScrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+(function() {
+    var oFixed = document.getElementById("fixed");
+    oFixed.onclick = function () {
+         timer = setInterval(function () {
+             iScrollTop = document.body.scrollTop || document.documentElement.scrollTop;
             if (iScrollTop <= 0) {
                 clearInterval(timer);
             } else {
                 window.scrollTo(0, iScrollTop -= 100);
             }
         }, 50);
-};
-    var oPicture=document.getElementById("picture");
-    var oImg=oPicture.getElementsByTagName("img");
-    var oButton=document.getElementById("button");
-    var oLeft=oButton.getElementsByClassName("left")[0];
-    var nowIndex=0;
-    var oRight=oButton.getElementsByClassName("right")[0];
-    var timer;
-    function play() {
-         timer = setInterval(function () {
-            oRight.onclick();
-        }, 2000);
     }
-    play();
-    oPicture.onmouseover = function(){
-        clearInterval(timer);
-    };
-    oPicture.onmouseout = function(){
-        play();
-    };
-    oRight.onclick=oLeft.onclick=function(){
-        if(this==oLeft){
-          nowIndex--;
-            if(nowIndex==-1) {
-                nowIndex = oImg.length - 1;
-               }
-            }else{
-                nowIndex++;
-                if(nowIndex==oImg.length ){
-                 nowIndex=0;
-                }
-            }
-        for(var i=0;i<oImg.length;i++){
-            oImg[i].className="";
-        }
-        oImg[nowIndex].className="selected";
-};
-
-
-
-
 })();
